8ca7111bed181f49229432d2d601a56fd70f7607
Megha Gupta <gupta34@email.sc.edu>
2016-09-16 16:09:10 -0700

    Abstract app module  in folder structure

---
+++ app/app.module.js
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview Basic random quote displaying angular2 app
+ * @author <a href="mailto:megha.gupta2205@gmail.com">Megha Gupta</a>
+ */
+
+ // IIFE function 
+(function(app) {
+	// all variables are angular2 component, modules, platform 
+	var NgModule  = ng.core.NgModule;
+	var BrowserModule = ng.platformBrowser.BrowserModule ;
+	var QuoteService = app.QuoteService;
+	var RandomQuoteComponent = app.RandomQuoteComponent;
+	var AppComponent = app.AppComponent;
+	// Every app should have module and components, odule and provider dependency 
+	//  Imports => Modules, Declaratiosn=> all components, Providers=> All services,, Bootstarp=> root component 
+  var AppModule = NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent, RandomQuoteComponent],
+    providers: [QuoteService],
+    bootstrap: [AppComponent]
+  })
+  .Class({
+    constructor: function() { }
+  });
+
+})(window.app || (window.app ={}));
